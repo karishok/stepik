a4c62faf7d9028dbe5d7d3d1234347da94bba906
Karishok <babaliants@bk.ru>
2020-09-16 20:27:47 +0300

    lesson 3.6

---
+++ test_items.py
@@ -0,0 +1,8 @@
+import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_choselanguage(browser):
+    browser.get(link)
+    time.sleep(30)
+    assert browser.find_element_by_class_name("btn-add-to-basket").is_displayed()
